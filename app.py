f00d69a8fe04cba05a83c661db86110c262c23cc
qianyuWang <wang18786872686@163.com>
2023-05-10 20:51:20 +0800

    Add base template and error template

--- app.py
+++ app.py
@@ -69,8 +69,29 @@ def forge():
     db.session.commit()
     click.echo('Done.')
 
+# @app.route('/')
+# def index():
+#     user = User.query.first()  # 读取用户记录
+#     movies = Movie.query.all()  # 读取所有电影记录
+#     return render_template('index.html', user=user, movies=movies)
+#
+# @app.errorhandler(404)  # 传入要处理的错误代码
+# def page_not_found(e):  # 接受异常对象作为参数
+#     user = User.query.first()
+#     return render_template('404.html', user=user), 404  # 返回模板和状态码
+
+@app.context_processor
+def inject_user():
+    user = User.query.first()
+    return dict(user=user)
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
 @app.route('/')
 def index():
-    user = User.query.first()  # 读取用户记录
-    movies = Movie.query.all()  # 读取所有电影记录
-    return render_template('index.html', user=user, movies=movies)
+    movies = Movie.query.all()
+    return render_template('index.html', movies=movies)
